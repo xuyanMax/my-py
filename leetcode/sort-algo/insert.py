4c492e3aaa3ef4367569af13dffcc800120bac2b
pete <xu.yan11@icloud.com>
2023-07-02 02:48:26 +0800

    added heapsort, insert

--- leetcode/sort-algo/insert.py
+++ leetcode/sort-algo/insert.py
@@ -5,7 +5,7 @@ class insert(object):
             if nums[i - 1] > nums[i]:
                 flag = nums[i]
                 j = i - 1
-                # nums[0:j] is sorted and try to insert nums[j] to a correct position
+                # nums[0:j] is sorted and try to insert nums[j] to output correct position
                 while j >= 0 and flag < nums[j]:
                     nums[j + 1] = nums[j]
                     j -= 1
